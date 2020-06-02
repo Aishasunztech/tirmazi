1f129cc514d99262b5410157fadfa5e802008682
Malik Farooq <malikfarooq4321@gmail.com>
2020-06-02 13:13:58 +0500

    Bugs fixed

--- app/src/main/java/com/sunztech/sahihbukhari/MyApplication.java
+++ app/src/main/java/com/sunztech/sahihbukhari/MyApplication.java
@@ -5,6 +5,10 @@ import android.app.Application;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class MyApplication extends Application {
 
     public static int numberOfClicks = 1;
@@ -12,6 +16,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         // register to be informed of activities starting up
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
@@ -20,9 +25,9 @@ public class MyApplication extends Application {
             public void onActivityCreated(Activity activity,
                                           Bundle savedInstanceState) {
 
-                // new activity created; force its orientation to portrait
+             /*   // new activity created; force its orientation to portrait
                 activity.setRequestedOrientation(
-                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);*/
 
             }
 
