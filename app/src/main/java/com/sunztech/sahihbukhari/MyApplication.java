367005ea7d1b3298e69831118ffca8b0adcb2ded
saudi <saud.vectorcoder@gmail.com>
2020-06-02 15:01:10 +0500

    Integrated OneSignal

--- app/src/main/java/com/sunztech/sahihbukhari/MyApplication.java
+++ app/src/main/java/com/sunztech/sahihbukhari/MyApplication.java
@@ -6,6 +6,7 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import com.crashlytics.android.Crashlytics;
+import com.onesignal.OneSignal;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -17,6 +18,11 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
+
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
 
         // register to be informed of activities starting up
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
